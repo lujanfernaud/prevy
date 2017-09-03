9057e2ef61d7be7878cbae124a9f34aba56a8e1a
lujanfernaud <lujanfernaud@gmail.com>
2017-09-03 16:13:47 +0100

    Add Events controller and views

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,53 @@
+class EventsController < ApplicationController
+  include ApplicationHelper
+
+  before_action :find_event, only: [:show, :edit, :update]
+
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.organized_events.build(event_params)
+
+    if @event.save
+      flash[:success] = "Event successfully created."
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update_attributes(event_params)
+      flash[:success] = "Event updated."
+      redirect_to event_path(@event)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+    def find_event
+      @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit(:title, :description,
+                                    :start_date, :end_date)
+    end
+end
