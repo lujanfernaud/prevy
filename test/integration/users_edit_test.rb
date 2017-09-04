94080c2473686fda3a0a90cc4cd205eaca767a40
lujanfernaud <lujanfernaud@gmail.com>
2017-09-04 23:02:01 +0100

    Refactor integration test for users edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -8,62 +8,68 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   test "update with valid name" do
     visit edit_user_path(@user)
 
-    fill_in "Name", with: "Philip"
-
+    fill_in  "Name", with: "Philip"
     click_on "Update"
 
-    assert page.has_content? "updated"
+    assert_valid
   end
 
   test "update with valid email" do
     visit edit_user_path(@user)
 
-    fill_in "Email", with: "philip@sample.com"
-
+    fill_in  "Email", with: "philip@sample.com"
     click_on "Update"
 
-    assert page.has_content? "updated"
+    assert_valid
   end
 
   test "update with valid password" do
     visit edit_user_path(@user)
 
-    fill_in "Password",              with: "more secure password"
-    fill_in "Password confirmation", with: "more secure password"
-
+    fill_in  "Password",              with: "more secure password"
+    fill_in  "Password confirmation", with: "more secure password"
     click_on "Update"
 
-    assert page.has_content? "updated"
+    assert_valid
   end
 
   test "update with invalid name" do
     visit edit_user_path(@user)
 
-    fill_in "Name", with: "Ph"
-
+    fill_in  "Name", with: "Ph"
     click_on "Update"
 
-    assert page.has_content? "error"
+    assert_invalid
   end
 
   test "update with invalid email" do
     visit edit_user_path(@user)
 
-    fill_in "Email", with: "philip@sample"
-
+    fill_in  "Email", with: "philip@sample"
     click_on "Update"
 
-    assert page.has_content? "error"
+    assert_invalid
   end
 
   test "update with invalid password" do
     visit edit_user_path(@user)
 
-    fill_in "Password",              with: "pass"
-    fill_in "Password confirmation", with: "pass"
-
+    fill_in  "Password",              with: "pass"
+    fill_in  "Password confirmation", with: "pass"
     click_on "Update"
 
-    assert page.has_content? "error"
+    assert_invalid
   end
+
+  private
+
+    def assert_valid
+      assert current_path, user_path(@user)
+      assert page.has_content? "updated"
+    end
+
+    def assert_invalid
+      assert current_path, edit_user_path(@user)
+      assert page.has_content? "error"
+    end
 end
