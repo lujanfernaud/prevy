d459271c8029996e2012c36b4dd0c9adee6eb118
lujanfernaud <lujanfernaud@gmail.com>
2017-09-11 17:01:14 +0100

    Rename css class in 'events_index' integration test

--- test/integration/events_index_test.rb
+++ test/integration/events_index_test.rb
@@ -4,6 +4,6 @@ class EventsIndexTest < ActionDispatch::IntegrationTest
   test "index shows events" do
     visit events_path
 
-    assert page.has_css? ".event-box", count: 12
+    assert page.has_css? ".box", count: 12
   end
 end
