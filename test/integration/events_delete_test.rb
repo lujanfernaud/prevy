b167638ea64bca10f17135c343fa3dbb702f0b8c
lujanfernaud <lujanfernaud@gmail.com>
2017-09-14 16:31:51 +0100

    Refactor integration test for events delete

--- test/integration/events_delete_test.rb
+++ test/integration/events_delete_test.rb
@@ -2,25 +2,27 @@ require 'test_helper'
 
 class EventsDeleteTest < ActionDispatch::IntegrationTest
   def setup
-    @user    = users(:phil)
+    @phil    = users(:phil)
+    @penny   = users(:penny)
     @event   = events(:one)
-    @invalid_user = users(:penny)
+    @address = addresses(:one)
+    @event.build_address(@address.attributes)
   end
 
   test "author can delete event" do
-    log_in_as(@user)
+    log_in_as(@phil)
     visit event_path(@event)
 
     assert_difference "Event.count", -1 do
       click_on "Delete event"
     end
 
-    assert current_path, user_path(@user)
+    assert current_path, user_path(@phil)
     assert page.has_content? "deleted"
   end
 
   test "user can't delete other users' event" do
-    log_in_as(@invalid_user)
+    log_in_as(@penny)
     visit event_path(@event)
 
     assert_not page.has_content? "Delete event"
