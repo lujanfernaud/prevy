868b9f56c9af39dc9e7c6adf1ec29d3012e1535c
lujanfernaud <lujanfernaud@gmail.com>
2018-03-21 21:02:25 +0000

    Sort notifications in reverse order

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @notifications = Notification.where(user: @user)
+    @notifications = Notification.where(user: @user).reverse
   end
 
   def destroy
