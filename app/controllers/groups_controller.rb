6d8e0aef7f4800537ad9b5aeeb541e4f52836400
lujanfernaud <lujanfernaud@gmail.com>
2018-04-05 09:43:40 +0100

    Order groups in groups index by creation date

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,6 +3,7 @@ class GroupsController < ApplicationController
 
   def index
     @groups = Group.where(hidden: false)
+                   .order(created_at: :desc)
                    .paginate(page: params[:page], per_page: 15)
   end
 
