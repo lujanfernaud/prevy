46da47dcd9b867ffee47d808d30b1dfa5f7ca600
lujanfernaud <lujanfernaud@gmail.com>
2018-04-07 09:04:16 +0100

    Add promotional section to home page

--- test/integration/home_page_test.rb
+++ test/integration/home_page_test.rb
@@ -9,6 +9,7 @@ class HomePageTest < ActionDispatch::IntegrationTest
   test "logged out user visits home page" do
     visit root_path
 
+    assert_promotional_section
     refute_upcoming_events
     assert_unhidden_groups
   end
@@ -18,6 +19,7 @@ class HomePageTest < ActionDispatch::IntegrationTest
 
     visit root_path
 
+    refute_promotional_section
     assert_upcoming_events
     refute page.has_content? "There are no upcoming events"
     assert page.has_link? "See more upcoming events"
@@ -29,12 +31,27 @@ class HomePageTest < ActionDispatch::IntegrationTest
 
     visit root_path
 
+    refute_promotional_section
     assert_upcoming_events
     assert page.has_content? "There are no upcoming events"
     assert_unhidden_groups
   end
 
   private
+
+    def assert_promotional_section
+      assert page.has_content? promotional_message
+      assert page.has_link? "Sign up and get started"
+    end
+
+    def refute_promotional_section
+      refute page.has_content? promotional_message
+      refute page.has_link? "Sign up and get started"
+    end
+
+    def promotional_message
+      "Create private events for your friends or family"
+    end
 
     def assert_upcoming_events
       assert page.has_css? ".upcoming-events-container"
