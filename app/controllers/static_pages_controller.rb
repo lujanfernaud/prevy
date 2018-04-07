73bb0024846c863874f2f984ab8312c7a66b13d6
lujanfernaud <lujanfernaud@gmail.com>
2018-04-07 10:02:30 +0100

    Refactor StaticPagesController

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,27 @@
 class StaticPagesController < ApplicationController
   def home
     if signed_in?
+      store_user_events
+      store_user_groups
+    end
+
+    store_unhidden_groups
+  end
+
+  private
+
+    def store_user_events
       @user         = User.find(current_user.id)
       user_events   = @user.events_from_groups
       @events_count = user_events.count
       @events       = EventDecorator.collection(user_events.limit(6))
+    end
+
+    def store_user_groups
       @user_groups = @user.groups
     end
 
-    @groups = Group.where(hidden: false).random_selection
-  end
+    def store_unhidden_groups
+      @unhidden_groups = Group.where(hidden: false).random_selection
+    end
 end
