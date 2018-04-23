eb6e07f5f8e90e5dd354e74d7619a9d49ffacc6c
lujanfernaud <lujanfernaud@gmail.com>
2018-04-23 08:09:46 +0100

    Organize test_helper configuration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ require 'simplecov'
 SimpleCov.start 'rails' unless ENV['NO_COVERAGE']
 
 ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'support/support'
@@ -9,23 +10,26 @@ require 'support/support'
 require 'capybara/rails'
 require 'capybara/minitest'
 require 'capybara-screenshot/minitest'
+
 Capybara.match = :prefer_exact
 Capybara.asset_host = 'http://localhost:3000'
-
-require 'minitest/autorun'
-require 'minitest/reporters'
-
-require 'webmock/minitest'
-WebMock.disable_net_connect!(allow_localhost: true)
-
-require 'sucker_punch/testing/inline'
-Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new, Minitest::Reporters::DefaultReporter.new]
 
 Capybara::Webkit.configure do |config|
   config.allow_url("gravatar.com")
   config.allow_url("api.mapbox.com")
   config.allow_url("api.tiles.mapbox.com")
 end
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new, Minitest::Reporters::DefaultReporter.new]
+
+require 'webmock/minitest'
+
+WebMock.disable_net_connect!(allow_localhost: true)
+
+require 'sucker_punch/testing/inline'
 
 class ActionDispatch::IntegrationTest
   include Capybara::DSL
