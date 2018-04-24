56646d2d2a490ec764650ffa994c5ca4719efcda
lujanfernaud <lujanfernaud@gmail.com>
2018-04-24 09:49:24 +0100

    Disallow sample groups and events to be edited

--- app/helpers/user_memberships_helper.rb
+++ app/helpers/user_memberships_helper.rb
@@ -7,6 +7,14 @@ module UserMembershipsHelper
     pluralize_with_count("Member", scope: group.members_with_role)
   end
 
+  def edit_group_link(group)
+    if group.sample_group?
+      link_to "Edit group", "#", class: "text-muted"
+    else
+      link_to "Edit group", edit_group_path(group)
+    end
+  end
+
   private
 
     def pluralize_with_count(title, scope:)
