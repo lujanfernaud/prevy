9221087b610ca4abe3f5e5b51db526a3497ca274
lujanfernaud <lujanfernaud@gmail.com>
2018-04-24 21:13:50 +0100

    Show 'there are no members' in group view

--- app/helpers/group_counters_helper.rb
+++ app/helpers/group_counters_helper.rb
@@ -6,14 +6,10 @@ module GroupCountersHelper
   end
 
   def members_title_with_count
-    return if @group.members_with_role.count.zero?
-
     members_title + role_count(@group.members_with_role.count)
   end
 
   def members_title
-    return if @group.members_with_role.count.zero?
-
     "Member".pluralize(@group.members_with_role.count)
   end
 
