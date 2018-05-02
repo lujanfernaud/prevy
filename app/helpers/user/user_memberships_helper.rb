3aae59a0ae63fb11867d5cd45fa599b0b2f86b59
lujanfernaud <lujanfernaud@gmail.com>
2018-05-02 20:40:42 +0100

    Restyle user memberships ('My groups')

--- app/helpers/user/user_memberships_helper.rb
+++ app/helpers/user/user_memberships_helper.rb
@@ -1,4 +1,12 @@
 module User::UserMembershipsHelper
+  def group_name_with_link(group)
+    link_to group.name, group_path(group)
+  end
+
+  def user_is_organizer_but_not_owner(group, user)
+    user.has_role?(:organizer, group) && !group.owner == user
+  end
+
   def group_organizers(group)
     pluralize_with_count("Organizer", scope: group.organizers)
   end
@@ -7,12 +15,23 @@ module User::UserMembershipsHelper
     pluralize_with_count("Member", scope: group.members_with_role)
   end
 
+  def links_for_owned_group(group)
+    edit_group_link(group)
+  end
+
   def edit_group_link(group)
     if group.sample_group?
       link_to "Edit group", "#", class: "text-muted"
     else
       link_to "Edit group", edit_group_path(group)
     end
+  end
+
+  def links_for_group_with_membership(group)
+    link_to "Cancel membership",
+      group_member_path(group, current_user),
+      method: :delete,
+      data: { confirm: "Are you sure to cancel your membership to '#{group.name}'?"}
   end
 
   private
