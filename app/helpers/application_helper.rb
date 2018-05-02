1f3899158921fe710b670b1aac41418a056601f1
lujanfernaud <lujanfernaud@gmail.com>
2018-05-02 08:13:32 +0100

    Position search bar below header in home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,10 @@ module ApplicationHelper
   include Application::HeaderHelpers
   include Application::AlertHelpers
 
+  def home_page?
+    controller_name == "static_pages" && action_name == "home"
+  end
+
   def breadcrumbs_separator
     "<span class='breadcrumbs-separator'> / </span>"
   end
