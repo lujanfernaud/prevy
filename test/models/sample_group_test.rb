19e399bd0b9422b857ca5eed576dff78006b9d3a
lujanfernaud <lujanfernaud@gmail.com>
2018-05-23 08:43:54 +0100

    Fix sample group test
    We only care about the number of sample users in the group.

--- test/models/sample_group_test.rb
+++ test/models/sample_group_test.rb
@@ -13,14 +13,14 @@ class SampleGroupTest < ActiveSupport::TestCase
     SampleGroup.create_for_user(user)
     group = Group.last
 
-    members_count = SampleUser.collection_for_sample_group.count
-    organizers_count = group.organizers.count - GROUP_OWNER_COUNT
-    members_with_role_count = members_count - organizers_count
+    sample_users_collection = SampleUser.collection_for_sample_group
+    organizers_count        = group.organizers.count - GROUP_OWNER_COUNT
+    members_with_role_count = sample_users_collection.count - organizers_count
 
     assert group.sample_group?
     assert_equal user, group.owner
 
-    assert_equal SampleUser.collection_for_sample_group, group.members
+    assert_equal sample_users_collection.count, group.members.count
     assert_equal members_with_role_count, group.members_with_role.count
 
     assert_equal 5, group.organizers.count
