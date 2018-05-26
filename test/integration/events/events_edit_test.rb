5fe387567f4c881177e90e32f82e4cc577863b89
lujanfernaud <lujanfernaud@gmail.com>
2018-05-26 21:18:29 +0100

    Refactor EventsUpdateTest

--- test/integration/events/events_edit_test.rb
+++ test/integration/events/events_edit_test.rb
@@ -12,12 +12,12 @@ class EventsUpdateTest < ActionDispatch::IntegrationTest
   test "update event with valid title" do
     visit_edit_group_event_path
 
-    fill_in "Title",       with: @event.title
+    fill_in "Title", with: @event.title
     find("trix-editor").click.set(@event.description)
     select "Japan", from: "Country"
     select_date_and_time @event.start_date, from: "event_start_date"
     select_date_and_time @event.end_date,   from: "event_end_date"
-    attach_file "event_image", "test/fixtures/files/sample.jpg"
+    attach_valid_image_for "event_image"
 
     click_on_update_event
     assert_valid
@@ -26,12 +26,12 @@ class EventsUpdateTest < ActionDispatch::IntegrationTest
   test "update event with valid description" do
     visit_edit_group_event_path
 
-    fill_in "Title",       with: @event.title
+    fill_in "Title", with: @event.title
     find("trix-editor").click.set(Faker::Lorem.paragraph)
     select "Japan", from: "Country"
     select_date_and_time @event.start_date, from: "event_start_date"
     select_date_and_time @event.end_date,   from: "event_end_date"
-    attach_file "event_image", "test/fixtures/files/sample.jpg"
+    attach_valid_image_for "event_image"
 
     click_on_update_event
     assert_valid
@@ -40,12 +40,12 @@ class EventsUpdateTest < ActionDispatch::IntegrationTest
   test "update event with valid date" do
     visit_edit_group_event_path
 
-    fill_in "Title",       with: @event.title
+    fill_in "Title", with: @event.title
     find("trix-editor").click.set(@event.description)
     select "Japan", from: "Country"
     select_date_and_time @event.start_date + 1.day, from: "event_start_date"
     select_date_and_time @event.end_date + 1.day,   from: "event_end_date"
-    attach_file "event_image", "test/fixtures/files/sample.jpg"
+    attach_valid_image_for "event_image"
 
     click_on_update_event
     assert_valid
@@ -54,11 +54,11 @@ class EventsUpdateTest < ActionDispatch::IntegrationTest
   test "update event with invalid start date" do
     visit_edit_group_event_path
 
-    fill_in "Title",       with: @event.title
+    fill_in "Title", with: @event.title
     find("trix-editor").click.set(@event.description)
     select_date_and_time 1.week.ago,              from: "event_start_date"
     select_date_and_time @event.end_date + 1.day, from: "event_end_date"
-    attach_file "event_image", "test/fixtures/files/sample.jpg"
+    attach_valid_image_for "event_image"
 
     click_on_update_event
     assert_invalid
@@ -67,11 +67,11 @@ class EventsUpdateTest < ActionDispatch::IntegrationTest
   test "update event with invalid end date" do
     visit_edit_group_event_path
 
-    fill_in "Title",       with: @event.title
+    fill_in "Title", with: @event.title
     find("trix-editor").click.set(@event.description)
     select_date_and_time @event.start_date, from: "event_start_date"
     select_date_and_time 1.week.ago,        from: "event_end_date"
-    attach_file "event_image", "test/fixtures/files/sample.jpg"
+    attach_valid_image_for "event_image"
 
     click_on_update_event
     assert_invalid
