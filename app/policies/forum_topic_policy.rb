67ecadc04da3d8d5245d6292b45dfa71e1399a43
lujanfernaud <lujanfernaud@gmail.com>
2018-05-29 13:01:25 +0100

    Add forum policies

---
+++ app/policies/forum_topic_policy.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+class ForumTopicPolicy < ApplicationPolicy
+  def index?
+    is_member? || is_group_owner?
+  end
+
+  def show?
+    is_member? || is_group_owner?
+  end
+
+  def create?
+    logged_in? && is_member_or_group_owner? && user.confirmed?
+  end
+
+  def update?
+    is_author? || is_group_owner_or_organizer?
+  end
+
+  def destroy?
+    is_author? || is_group_owner_or_organizer?
+  end
+
+  private
+
+    def is_member?
+      group.members.include? user
+    end
+
+    def is_member_or_group_owner?
+      is_member? || is_group_owner?
+    end
+
+    def is_group_owner?
+      group.owner == user
+    end
+
+    def is_author?
+      record.user == user
+    end
+
+    def is_group_owner_or_organizer?
+      is_group_owner? || is_group_organizer?
+    end
+
+    def is_group_organizer?
+      group.organizers.include? user
+    end
+
+    def group
+      @_group ||= Group.find(params[:group_id])
+    end
+end
