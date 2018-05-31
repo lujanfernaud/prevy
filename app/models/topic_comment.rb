837114c1a203396ac6687416a3ca8cc1b9d2678f
lujanfernaud <lujanfernaud@gmail.com>
2018-05-31 08:57:58 +0100

    Touch Topic from TopicComment

--- app/models/topic_comment.rb
+++ app/models/topic_comment.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TopicComment < ApplicationRecord
-  belongs_to :topic
+  belongs_to :topic, touch: true
   belongs_to :user
 
   validates :body, presence: true, length: { minimum: 3 }
