4cde9da8504784d04c80d73b6c01bc2fee92a711
lujanfernaud <lujanfernaud@gmail.com>
2018-06-07 07:24:31 +0100

    Refactor TopicsDeletionTest

--- test/integration/groups/topics/topics_deletion_test.rb
+++ test/integration/groups/topics/topics_deletion_test.rb
@@ -1,17 +1,13 @@
 require 'test_helper'
 
 class TopicsDeletionTest < ActionDispatch::IntegrationTest
-  include TopicsIntegrationSupport
-
   def setup
     @group = groups(:one)
     @phil  = users(:phil)
-
-    prepare_javascript_driver
   end
 
   test "author deletes topic" do
-    topic = fake_topic
+    topic = fake_topic(group: @group, user: @phil)
     topic.save
 
     log_in_as @phil
@@ -22,15 +18,4 @@ class TopicsDeletionTest < ActionDispatch::IntegrationTest
 
     assert page.has_content? "Topic deleted."
   end
-
-  private
-
-    def fake_topic(params = {})
-      Topic.new(
-        group: @group,
-        user:  @phil,
-        title: params[:title] || "Fake topic",
-        body:  params[:body]  || "Body of the fake topic."
-      )
-    end
 end
