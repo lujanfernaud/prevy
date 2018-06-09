de3c08db4cadbae1989d9dff0ffd13f81567c639
lujanfernaud <lujanfernaud@gmail.com>
2018-06-09 12:09:49 +0100

    Add missing tests to Groups::RolesControllerTest

--- test/controllers/groups/roles_controller_test.rb
+++ test/controllers/groups/roles_controller_test.rb
@@ -13,4 +13,26 @@ class Groups::RolesControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
   end
+
+  test "should create role" do
+    @user.remove_role :organizer, @group
+
+    sign_in @user
+
+    post group_roles_url(@group, user_id: @user, role: "organizer")
+
+    assert @user.has_role? :organizer, @group
+    assert_redirected_to group_roles_url(@group)
+  end
+
+  test "should destroy role" do
+    @user.add_role :organizer, @group
+
+    sign_in @user
+
+    delete group_role_url(@group, @user, role: "organizer")
+
+    refute @user.has_role? :organizer, @group
+    assert_redirected_to group_roles_url(@group)
+  end
 end
