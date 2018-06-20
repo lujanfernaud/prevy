c17ea396225d475030f28ad8edc4bd94ea67ffd6
lujanfernaud <lujanfernaud@gmail.com>
2018-06-20 11:53:48 +0100

    Counter cache user comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180619061020) do
+ActiveRecord::Schema.define(version: 20180620104806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -203,6 +203,7 @@ ActiveRecord::Schema.define(version: 20180619061020) do
     t.boolean "sample_user", default: false
     t.boolean "admin", default: false
     t.string "slug"
+    t.integer "topic_comments_count", default: 0, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug"
