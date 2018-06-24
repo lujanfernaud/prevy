6f812010d1949954aa65c4779950eec5169d2d98
lujanfernaud <lujanfernaud@gmail.com>
2018-06-24 09:07:41 +0100

    Add comments to routes and reorganize

--- config/routes.rb
+++ config/routes.rb
@@ -11,40 +11,60 @@ Rails.application.routes.draw do
     get   "logout"  => "devise/sessions#destroy"
   end
 
+  # Root
   root "static_pages#home"
 
+  # Users
   resources :users, only: [:show, :edit, :update] do
-    resources :events,              only: [:index],
-      controller: "users/events"
+    # User Groups (My Groups)
+    get "groups", to: "users/memberships#index"
+
+    # User Membership Requests
     resources :membership_requests, only: [:index, :show],
       controller: "users/membership_requests"
-    resources :notifications,       only: [:index, :destroy],
+
+    # User Events
+    resources :events, only: [:index], controller: "users/events"
+
+    # User Notifications
+    resources :notifications, only: [:index, :destroy],
       controller: "users/notifications"
 
-    get  "groups",                  to: "users/memberships#index"
     get  "notification_redirecter", to: "users/notification_redirecters#new"
     get  "notification_settings",   to: "users/notifications#edit"
     put  "notification_settings",   to: "users/notifications#update"
     post "notification_cleaners",   to: "users/notification_cleaners#create"
   end
 
+  # Groups
   resources :groups do
+    # Group Members
     resources :memberships, as: :members, path: "members",
       only: [:index, :create, :destroy], controller: "groups/memberships"
-    resources :membership_requests,       only: [:new, :create, :destroy],
-      controller: "groups/membership_requests"
-    resources :roles,                     only: [:index, :create, :destroy],
+
+    # Group Roles
+    resources :roles, only: [:index, :create, :destroy],
       controller: "groups/roles"
+
+    # Group Membership Requests
+    resources :membership_requests, only: [:new, :create, :destroy],
+      controller: "groups/membership_requests"
+
+    # Group Events
     resources :events
 
+    # Group Topics
     resources :topics, controller: "groups/topics" do
       resources :comments, controller: "groups/topics/comments", shallow: true
     end
   end
 
+  # Events
   resources :events do
+    # Event Attendees
     resources :attendances, controller: "events/attendances"
   end
 
+  # Search
   resource :search, only: :show
 end
