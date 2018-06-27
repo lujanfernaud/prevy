f8f8b895ae7248f938d4c3f307f2c072ea3efb1e
lujanfernaud <lujanfernaud@gmail.com>
2018-06-27 08:44:55 +0100

    Remove password_digest column from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180621083136) do
+ActiveRecord::Schema.define(version: 20180627073834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -193,7 +193,6 @@ ActiveRecord::Schema.define(version: 20180621083136) do
     t.string "email", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
     t.string "location"
     t.string "bio"
     t.jsonb "settings", default: {}, null: false
