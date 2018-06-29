8c140b45fea5551ff285bb2557df4bec2a91f794
lujanfernaud <lujanfernaud@gmail.com>
2018-06-29 16:59:55 +0100

    Add missing design to group events index

--- test/integration/events/events_index_test.rb
+++ test/integration/events/events_index_test.rb
@@ -19,7 +19,23 @@ class EventsIndexTest < ActionDispatch::IntegrationTest
 
     visit user_events_path(@onitsuka)
 
-    refute page.has_css? ".box"
+    refute page.has_css?     ".box"
     assert page.has_content? "There are no upcoming events"
+  end
+
+  test "user visits group events index" do
+    group = groups(:one)
+
+    log_in_as(@phil)
+
+    visit group_events_path(group)
+
+    within ".breadcrumb" do
+      assert page.has_link?    group.name
+      assert page.has_content? "Events"
+    end
+
+    assert page.has_content? "Events"
+    assert page.has_css?     ".box"
   end
 end
