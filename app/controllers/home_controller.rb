f03ffdf98431b7cc2114348f38ee8b6cb7865809
lujanfernaud <lujanfernaud@gmail.com>
2018-07-17 11:31:54 +0100

    Refactor HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@
 class HomeController < ApplicationController
   def index
     if signed_in?
+      @user = find_user
+
       store_user_events
       store_user_groups
     end
@@ -12,17 +14,21 @@ class HomeController < ApplicationController
 
   private
 
+    def find_user
+      @user = User.find(current_user.id)
+    end
+
     def store_user_events
-      @user         = User.find(current_user.id)
       user_events   = @user.events_from_groups.includes(:image_placeholder)
-      @events_count = user_events.count
+      @events_count = user_events.size
       @events       = EventDecorator.collection(user_events.limit(6))
     end
 
     def store_user_groups
       @user_groups = @user.groups
                           .includes(:image_placeholder)
-                          .order(:updated_at).reverse
+                          .order(updated_at: :desc)
+                          .distinct
     end
 
     def store_unhidden_groups
