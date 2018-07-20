7af602bedc0134d37f5c6ae3a198eb70f7fd8722
lujanfernaud <lujanfernaud@gmail.com>
2018-07-20 09:43:38 +0100

    Do not show organizers count in 'my groups'

--- app/helpers/user/user_memberships_helper.rb
+++ app/helpers/user/user_memberships_helper.rb
@@ -10,23 +10,9 @@ module User::UserMembershipsHelper
       !user.owned_groups.include?(group)
   end
 
-  def group_organizers(group)
-    pluralize_with_count("Organizer", scope: group.organizers)
-  end
-
-  def group_members(group)
-    pluralize_with_count("Member", scope: group.members_with_role)
-  end
-
-  def pluralize_with_count(title, scope:)
-    count = scope.size
-
-    title.pluralize(count) + ": #{count}"
-  end
-
   def edit_group_link(group)
     if group.sample_group?
-      link_to "Edit group", "#", class: "text-muted"
+      link_to "Edit group", "", class: "text-muted"
     else
       link_to "Edit group", edit_group_path(group)
     end
