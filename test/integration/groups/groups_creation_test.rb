91b2d1e41ab708288dc4241ebb4cfce610302def
lujanfernaud <lujanfernaud@gmail.com>
2018-07-28 21:31:37 +0100

    Add missing tests for groups creation and edition

--- test/integration/groups/groups_creation_test.rb
+++ test/integration/groups/groups_creation_test.rb
@@ -38,6 +38,30 @@ class GroupsCreationTest < ActionDispatch::IntegrationTest
     assert page.has_content? "Yay! You created a group!"
   end
 
+  test "user tries to create a group with wrong data" do
+    prepare_javascript_driver
+
+    user = users(:phil)
+
+    log_in_as(user)
+
+    visit new_group_path
+
+    fill_in "Name",     with: "T"
+    fill_in "Location", with: Faker::Address.city
+    fill_in_description(Faker::Lorem.paragraph)
+    attach_valid_image_for "group_image"
+
+    choose "group_hidden_false"
+    choose "group_all_members_can_create_events_false"
+
+    within "form" do
+      click_on "Create group"
+    end
+
+    assert page.has_content? "error"
+  end
+
   test "user with unconfirmed account tries to create a group" do
     user = users(:unconfirmed)
 
--- test/integration/groups/groups_edit_test.rb
+++ test/integration/groups/groups_edit_test.rb
@@ -35,4 +35,27 @@ class GroupsEditTest < ActionDispatch::IntegrationTest
 
     assert page.has_content? "The group was deleted."
   end
+
+  test "user edits a group with wrong data" do
+    prepare_javascript_driver
+
+    user  = users(:phil)
+    group = groups(:one)
+
+    log_in_as(user)
+    visit edit_group_path(group)
+
+    fill_in "Name",     with: "Test group"
+    fill_in "Location", with: "C"
+    fill_in_description(Faker::Lorem.paragraph)
+
+    attach_valid_image_for "group_image"
+
+    choose "group_hidden_true"
+    choose "group_all_members_can_create_events_true"
+
+    click_on "Update group"
+
+    assert page.has_content? "error"
+  end
 end
