d60459b666a5871699b5614b688a802bfe62e9ab
lujanfernaud <lujanfernaud@gmail.com>
2018-07-29 08:23:20 +0100

    Refactor Events::AttendancesController
    Remove unneeded condition in #create action.

--- app/controllers/events/attendances_controller.rb
+++ app/controllers/events/attendances_controller.rb
@@ -3,26 +3,22 @@
 class Events::AttendancesController < ApplicationController
   include ApplicationHelper
 
-  after_action :verify_authorized
+  before_action :find_event
+  after_action  :verify_authorized
 
   def create
-    @event = find_event
     @attendance = new_attendance
 
     authorize @attendance
 
-    if @attendance.save
-      flash[:success] = "Yay! You are attending this event!"
-    else
-      flash[:danger] = "There has been an error. " \
-        "Please refresh the page and try again."
-    end
+    @attendance.save
+
+    flash[:success] = "Yay! You are attending this event!"
 
     redirect_to group_event_path(@event.group, @event)
   end
 
   def destroy
-    @event = find_event
     @attendance = find_attendance
 
     authorize @attendance
@@ -37,20 +33,20 @@ class Events::AttendancesController < ApplicationController
   private
 
     def find_event
-      Event.find(params[:event_id])
+      @event = Event.find(params[:event_id])
     end
 
     def new_attendance
       Attendance.new(
         attended_event_id: @event.id,
-        attendee_id: current_user.id
+        attendee_id:       current_user.id
       )
     end
 
     def find_attendance
       Attendance.find_by(
         attended_event_id: @event.id,
-        attendee_id: current_user.id
+        attendee_id:       current_user.id
       )
     end
 end
